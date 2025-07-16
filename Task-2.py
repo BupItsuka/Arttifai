053508a2a4b362c88d554643570c3bb072509899
Bup Itsuka <120076035+BupItsuka@users.noreply.github.com>
2025-07-16 09:43:28 +0530

    Task-2.py

--- Task-2.py
+++ Task-2.py
@@ -1,25 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class ToDoApp:
+class ToDo:
     def __init__(self, root):
         self.root = root
         self.root.title("To-Do List")
-
         self.tasks = []
-
         self.task_entry = tk.Entry(root, width=40)
         self.task_entry.pack(pady=10)
-
         self.add_button = tk.Button(root, text="Add Task", width=20, command=self.add_task)
         self.add_button.pack()
-
         self.task_listbox = tk.Listbox(root, width=50, selectmode=tk.SINGLE)
         self.task_listbox.pack(pady=10)
-
         self.complete_button = tk.Button(root, text="Mark as Completed", command=self.complete_task)
         self.complete_button.pack(pady=2)
-
         self.remove_button = tk.Button(root, text="Remove Task", command=self.remove_task)
         self.remove_button.pack(pady=2)
 
@@ -53,5 +47,5 @@ class ToDoApp:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ToDoApp(root)
+    app = ToDo(root)
     root.mainloop()
